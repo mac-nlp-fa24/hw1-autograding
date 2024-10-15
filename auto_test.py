ca87def17b1df9a5eac0f6fa62655439d066cb4e
Suhas Arehalli <SArehalli@users.noreply.github.com>
2024-10-14 23:58:06 -0500

    initial commit

---
+++ auto_test.py
@@ -0,0 +1,32 @@
+import subprocess
+import os
+from pathlib import Path
+
+self_dir = "./auto-tests/"
+
+test_folder = "cases/"
+undef = ["ppl: nan\n", "ppl: inf\n"]
+
+def test_match(testnum): 
+    with open(testnum.format("in")) as in_f, open(testnum.format("out")) as out_f:
+        out = subprocess.run(in_f.read().split(), capture_output=True)
+        correct = out_f.read()
+        if correct in undef:
+            assert(out.stdout.decode() in undef)
+        else:
+            assert(out.stdout.decode() == correct)
+
+def pytest_generate_tests(metafunc):
+        examples = []
+        for fn in os.listdir(test_folder):
+            if fn.endswith("-out.txt"):
+                continue
+            testlabel = test_folder + fn.split("-")[0] + "-{}.txt"
+            examples.append(testlabel)
+
+        metafunc.parametrize("testnum", examples)
+
+
+
+
+
---
+++ generate_tests.py
@@ -0,0 +1,66 @@
+import subprocess
+import os
+from pathlib import Path
+
+self_dir = "./auto-tests/"
+
+run_cmd = "python main.py --train {} --valid {} --smooth {} -n {}"
+discount = " --discount {}" 
+lambdas = " --coeff {}"
+
+output_dir = self_dir + "cases/"
+test_folder = self_dir + "data/"
+
+def save(path, output, cmd, i):
+    print(output)
+    Path(path).mkdir(parents=True, exist_ok=True) 
+    with open(path + "test{}-in.txt".format(i), "w") as out_f:
+        out_f.write(cmd)
+    with open(path + "test{}-out.txt".format(i), "w") as out_f:
+        out_f.write(output.stdout.decode())
+
+
+coeffss = {3: [[0.5, 0.25, 0.25], 
+              [0.34, 0.33, 0.33],
+              [0.75, 0.25, 0.0]],
+          4: [[0.6, 0.2, 0.1, 0.1],
+              [0.5, 0.25, 0.125, 0.125],
+              [0.8, 0.1, 0.1, 0.0]]
+          }
+
+discounts = [0.3, 0.7, 0.9]
+
+i = 0
+for fn in os.listdir(test_folder):
+    path = test_folder + fn
+    for n in [1, 2, 3, 4]:
+        # MLE
+        mle_cmd = run_cmd.format(path + "/train.txt", path + "/test.txt", "mle", n) 
+        out = subprocess.run(mle_cmd.split(), capture_output=True)
+        save(output_dir, out, mle_cmd, i)
+        i += 1
+
+        # laplace
+        l_cmd = run_cmd.format(path + "/train.txt", path + "/test.txt", "laplace", n) 
+        out = subprocess.run(l_cmd.split(), capture_output=True)
+        save(output_dir, out, l_cmd, i)
+        i += 1
+
+    for n in [3, 4]:
+        # interpolation
+        for coeffs in coeffss[n]:
+            interp_cmd = run_cmd.format(path + "/train.txt", path + "/test.txt", "interp", n) + lambdas.format(",".join((str(x) for x in coeffs))) 
+            out = subprocess.run(interp_cmd.split(), capture_output=True)
+            save(output_dir, out, interp_cmd, i)
+            i += 1
+
+    for n in [3, 4]:
+        # backoff
+        for d in discounts:
+            bo_cmd = run_cmd.format(path + "/train.txt", path + "/test.txt", "backoff", n) + discount.format(d)
+            out = subprocess.run(bo_cmd.split(), capture_output=True)
+            save(output_dir, out, bo_cmd, i)
+            i += 1
+
+
+
