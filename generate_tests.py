b67c091b6df12c5ce544caf93dd534cb0c89e2c3
Suhas Arehalli <SArehalli@users.noreply.github.com>
2024-10-22 16:57:37 -0500

    add logs

--- generate_tests.py
+++ generate_tests.py
@@ -7,17 +7,19 @@ self_dir = "./hw1-autograding/"
 run_cmd = "python main.py --train {} --valid {} --smooth {} -n {}"
 discount = " --discount {}" 
 lambdas = " --coeff {}"
+debug = "--debug --byword".split()
 
 output_dir = self_dir + "cases/"
 test_folder = self_dir + "data/"
 
 def save(path, output, cmd, i):
-    print(output)
     Path(path).mkdir(parents=True, exist_ok=True) 
     with open(path + "test{}-in.txt".format(i), "w") as out_f:
         out_f.write(cmd)
     with open(path + "test{}-out.txt".format(i), "w") as out_f:
         out_f.write(output.stdout.decode())
+    with open(path + "test{}-log.txt".format(i), "w") as out_f:
+        out_f.write(output.stderr.decode())
 
 
 coeffss = {3: [[0.5, 0.25, 0.25], 
@@ -36,13 +38,13 @@ for fn in os.listdir(test_folder):
     for n in [1, 2, 3, 4]:
         # MLE
         mle_cmd = run_cmd.format(path + "/train.txt", path + "/test.txt", "mle", n) 
-        out = subprocess.run(mle_cmd.split(), capture_output=True)
+        out = subprocess.run(mle_cmd.split() + debug, capture_output=True)
         save(output_dir, out, mle_cmd, i)
         i += 1
 
         # laplace
         l_cmd = run_cmd.format(path + "/train.txt", path + "/test.txt", "laplace", n) 
-        out = subprocess.run(l_cmd.split(), capture_output=True)
+        out = subprocess.run(l_cmd.split() + debug, capture_output=True)
         save(output_dir, out, l_cmd, i)
         i += 1
 
@@ -50,7 +52,7 @@ for fn in os.listdir(test_folder):
         # interpolation
         for coeffs in coeffss[n]:
             interp_cmd = run_cmd.format(path + "/train.txt", path + "/test.txt", "interp", n) + lambdas.format(",".join((str(x) for x in coeffs))) 
-            out = subprocess.run(interp_cmd.split(), capture_output=True)
+            out = subprocess.run(interp_cmd.split() + debug, capture_output=True)
             save(output_dir, out, interp_cmd, i)
             i += 1
 
@@ -58,7 +60,7 @@ for fn in os.listdir(test_folder):
         # backoff
         for d in discounts:
             bo_cmd = run_cmd.format(path + "/train.txt", path + "/test.txt", "backoff", n) + discount.format(d)
-            out = subprocess.run(bo_cmd.split(), capture_output=True)
+            out = subprocess.run(bo_cmd.split() + debug, capture_output=True)
             save(output_dir, out, bo_cmd, i)
             i += 1
 
